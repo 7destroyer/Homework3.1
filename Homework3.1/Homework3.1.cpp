158c0796a307c51959ce7e12dea33fe10aea2825
Anton <antoha357357@gmail.com>
2025-02-09 14:37:44 +0300

    Add project files.

---
+++ Homework3.1/Homework3.1.cpp
@@ -0,0 +1,32 @@
+﻿//генерация случайного пароля
+#include <iostream>
+#include <string>
+#include <random>
+
+using namespace std;
+
+string generatePassword(int length, bool includeSymbols);
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    int length, includeSymbols;
+    cout << "Длина: " << endl;
+    cin >> length;
+    cout << "Символы: " << endl;
+    cin >> includeSymbols;
+    cout << "Пароль: " << generatePassword(length, includeSymbols) << endl;
+    return 0;
+}
+
+string generatePassword(int length, bool includeSymbols) {
+    const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    const string symbols = "!@#$%^&*()_+=-`~[]\{}|;':\",./<>?";
+    string res = chars;
+    res += includeSymbols ? symbols : "";
+    string p;
+    for (int i = 0; i < length; i++){
+        p += chars[rand() % chars.size()];
+    }
+    return p;
+}
